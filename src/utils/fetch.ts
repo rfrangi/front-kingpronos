33cd7d529decfc8459e66bf557740fe30ef5e17e
romain frangi <romain.frangi@cleverconnect.com>
2022-08-26 11:13:48 +0200

    [KingPronos] change conf fetch

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -2,10 +2,13 @@ import {HttpHeaders } from '@angular/common/http';
 
 export const URL_STOCKAGE = 'http://d11mhhwvxnv6xf.cloudfront.net/';
 
+/* Environnement Test */
 //export const URL_GATEWAY = 'https://protected-sierra-89708.herokuapp.com/';
-//export const URL_GATEWAY = 'https://kingpronos.herokuapp.com/';
 //export const URL_GATEWAY = `http://localhost:8888/`;
-export const URL_GATEWAY = 'https://costellopronos.herokuapp.com/';
+
+/* Environnement Prod */
+export const URL_GATEWAY = 'https://kingpronos.herokuapp.com/';
+//export const URL_GATEWAY = 'https://pierrickpronos.herokuapp.com/';
+//export const URL_GATEWAY = 'https://costellopronos.herokuapp.com/';
 
 export const HTTP_OPTIONS = { headers: new HttpHeaders({ 'Content-Type': 'application/json' })};
-
